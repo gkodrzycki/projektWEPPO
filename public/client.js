4c41a72745c179ea437ec2cb637b8661aa02ec99
Grzegorz Kodrzycki <95308491+gkodrzycki@users.noreply.github.com>
2023-01-29 14:57:24 +0100

    Comms, cleanup?

--- public/client.js
+++ public/client.js
@@ -1,86 +1,94 @@
-const socket = io('http://localhost:3000');
-const board = document.getElementById("gameContainer");
+const socket = io('http://localhost:3000')
+const board = document.getElementById("gameContainer")
 const roomContainer = document.getElementById("room-container")
 
+//If not in room there is no point for asking name
 if(board != null){
-    const name = prompt("What's your name?");
-    socket.emit('new-user', roomName, name);
+    const name = prompt("What's your name?")
+    socket.emit('new-user', roomName, name)
 }
 
-//Zebranie informacji po wyborze pola
+//Getting all info after choosing tile
 function onClickHandler(btn){   
-    if(possible[btn.getAttribute("cellId")] == ""){
-        if(!finished){
-            var args = [];
-        //btn player com finished possible newplayer winner room
-        args[0] = btn.getAttribute("cellId");
-        args[1] = currPlayer;
-        
-        cellChosen(args[0]);
-        checkwinner();
-        args[3] = finished;
+    if (possible[btn.getAttribute("cellId")] == "") {
+        if (!finished) {
+            var args = []
+            //btn player com finished possible newplayer winner room
+            args[0] = btn.getAttribute("cellId")
+            args[1] = currPlayer
+            
+            cellChosen(args[0])
+            checkwinner()
+            args[3] = finished
         }
+        
+        //If there are still possible moves change
         if(!finished)
-            changePalyer();
+            changePalyer()
 
         args[4] = possible
-        args[2] = "";
-        args[5] = currPlayer;
-        args[6] = winner;
-        args[7] = roomName;
-        socket.emit('changed', args);
+        args[2] = ""
+        args[5] = currPlayer
+        args[6] = winner
+        args[7] = roomName
+        socket.emit('changed', args)
     }
 }
 
-//Update stanu planszy
+//Update board state
 socket.on('buttonUpdate', function(data){
-    document.getElementById(data[0]).innerHTML = data[1];
-    document.getElementById("statusText").innerHTML = data[2];
-    currPlayer = data[5];
+    document.getElementById(data[0]).innerHTML = data[1]
+    document.getElementById("statusText").innerHTML = data[2]
+    currPlayer = data[5]
+    
     if(data[1] == "X")
-        document.getElementById(data[0]).style.backgroundColor = "red";
+        document.getElementById(data[0]).style.backgroundColor = "red"
     else
-        document.getElementById(data[0]).style.backgroundColor = "blue";
-    possible = data[4];
-    finished = data[3];
+        document.getElementById(data[0]).style.backgroundColor = "blue"
+    
+    possible = data[4]
+    finished = data[3]
 })
 
+//Get current board state 
 socket.on('getState', data => {
     for(let i = 0; i < 9; i++){
-        if(data[0][i] == "X"){
-            document.getElementById(i).style.backgroundColor = "red";
-            document.getElementById(i).textContent = "X";
-        }
-        else if(data[0][i] == "O"){
-            document.getElementById(i).style.backgroundColor = "blue";
-            document.getElementById(i).textContent = "O";
+        if (data[0][i] == "X") {
+            document.getElementById(i).style.backgroundColor = "red"
+            document.getElementById(i).textContent = "X"
+        } else if(data[0][i] == "O") {
+            document.getElementById(i).style.backgroundColor = "blue"
+            document.getElementById(i).textContent = "O"
         }
     }
-    document.getElementById("statusText").innerHTML = data[1];
+    document.getElementById("statusText").innerHTML = data[1]
 })
 
+//Manage clicked reset button
 function resetHandler(){
-    restartGame();
-    socket.emit('reset', roomName);
+    restartGame()
+    socket.emit('reset', roomName)
 }
 
+//Reser board state
 socket.on('updateReset', () => {
-    restartGame();
+    restartGame()
 })
 
 socket.on('message', () => {
-    window.alert("Pokój nie istnieje albo jest pusty");
+    window.alert("Pokój nie istnieje albo jest pusty")
 })
 
-
+//Handle creating new room
 socket.on('roomCreated', room => {
-    const roomLink = document.createElement('a');
-    const roomBlock = document.createElement('div');
-    roomLink.href = `/${room}`;
-    roomLink.innerText = `${room}`;
-    roomBlock.classList.add('cell');
-    roomLink.style.textDecoration = 'none';
-    roomBlock.appendChild(roomLink);
-    roomBlock.setAttribute("id", room);
-    roomContainer.append(roomBlock);
+    const roomLink = document.createElement('a')
+    const roomBlock = document.createElement('div')
+    roomLink.href = `/${room}`
+    roomLink.innerText = `${room}`
+    roomBlock.classList.add('cell')
+    roomLink.style.textDecoration = 'none'
+    roomLink.style.color = "inherit"
+    roomBlock.appendChild(roomLink)
+    roomBlock.setAttribute("id", room)
+    roomContainer.append(roomBlock)
 })
--- server.js
+++ server.js
@@ -1,145 +1,151 @@
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
 
-app.set('views', './views');
-app.set('view engine', 'ejs');
-app.use(express.static(('public')));   
-app.use(express.urlencoded({extended: true}));
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(express.static(('public')))   
+app.use(express.urlencoded({extended: true}))
 
-const rooms = {new:  {users: {}, player1: {}, player2: {}, move: {}, state: ["", "", "", "", "", "", "", "", ""], com: ""}};
+const rooms = {testRoom:  {users: {}, player1: {}, player2: {}, move: {}, state: ["", "", "", "", "", "", "", "", ""], comment: ""}}
 
+//Home page
 app.get('/', (req, res) => {
     res.render('index', {rooms: rooms})
-});
+})
 
 //Join room
 app.get('/:room', (req, res) => {
-    if(rooms[req.params.room] == null){
-        return res.redirect('/');
+    //If room doesn't exist there is nowhere to go
+    if (rooms[req.params.room] == null){
+        return res.redirect('/')
     }
     res.render('room', {roomName: req.params.room})
-});
+})
 
-//Create
+//Create room
 app.post('/room', (req, res) => {
-    if(rooms[req.body.room] != null){
-        return res.redirect('/');
+    //if room already exists there is no point to make 2nd with the same name
+    if (rooms[req.body.room] != null) {
+        return res.redirect('/')
     }
-    rooms[req.body.room] = {users: {}, player1: {}, player2: {}, move: {}, state: ["", "", "", "", "", "", "", "", ""], com: ""}
-    res.redirect('/');
+    rooms[req.body.room] = {users: {}, player1: {}, player2: {}, move: {}, state: ["", "", "", "", "", "", "", "", ""], comment: ""}
+    res.redirect('/')
     io.emit('roomCreated', req.body.room)
 })
 
 io.on('connection', (socket) => {
     socket.on('changed', data => {
-        var name2 = ""; 
-        if(Object.keys(rooms[data[7]].move)[0] == socket.id && Object.keys(rooms[data[7]].users).length >= 2){
-            rooms[data[7]].state = data[4];
+        var name2 = ""
+        //If there is any game we can update
+        if (Object.keys(rooms[data[7]].move)[0] == socket.id && Object.keys(rooms[data[7]].player1).length == 1 && Object.keys(rooms[data[7]].player2).length) {
+            rooms[data[7]].state = data[4]
 
-            if(socket.id == Object.keys(rooms[data[7]].player1)[0]){
-                rooms[data[7]].move = rooms[data[7]].player2;
+            //change who's move it is now
+            if (socket.id == Object.keys(rooms[data[7]].player1)[0]) {
+                rooms[data[7]].move = rooms[data[7]].player2
                 name2 =  Object.values(rooms[data[7]].player1)[0] 
-            }
-            if(socket.id == Object.keys(rooms[data[7]].player2)[0]){
-                rooms[data[7]].move = rooms[data[7]].player1; 
+            } else if(socket.id == Object.keys(rooms[data[7]].player2)[0]) {
+                rooms[data[7]].move = rooms[data[7]].player1 
                 name2 =  Object.values(rooms[data[7]].player2)[0] 
             }
 
-            if(data[6] == false && data[3] == false)
-                data[2] = Object.values(rooms[data[7]].move)[0] + "'s turn";
-            else if(data[6] == false && data[3] == true)
+            //check what comment should appear
+            if (!data[6] && !data[3])
+                data[2] = Object.values(rooms[data[7]].move)[0] + "'s turn"
+            else if (!data[6] && data[3])
                 data[2] = "Draw!"
             else
-                data[2] = name2 + " wins!";
+                data[2] = name2 + " wins!"
             
-                rooms[data[7]].com = data[2];
+            rooms[data[7]].comment = data[2]
+            //Update for everyone
             io.to(data[7]).emit('buttonUpdate', data)
         }
     })
 
     socket.on('reset', room => {
-        if(Object.keys(rooms[room].move)[0] == socket.id && Object.keys(rooms[room].users).length >= 2){
-            io.to(room).emit('updateReset');
+        //Check who want to reset
+        if (Object.keys(rooms[room].move)[0] == socket.id && Object.keys(rooms[room].users).length >= 2) {
+            io.to(room).emit('updateReset')
             rooms[room].move = rooms[room].player1
-            rooms[room].com = Object.values(rooms[room].move)[0] + "'s turn!";
-            rooms[room].state = ["", "", "", "", "", "", "", "", ""];
-            io.to(room).emit('getState', data);
-        }
-        else if(Object.keys(rooms[room].move)[0] != socket.id && Object.keys(rooms[room].users).length >= 2){
-            data =[rooms[room].state, rooms[room].com];
-            io.to(room).emit('getState', data);
-        }
-        else{
-            io.to(room).emit('updateReset');
-            rooms[room].com = "Waiting for opponent...";
-            rooms[room].state = ["", "", "", "", "", "", "", "", ""];
-            data =[rooms[room].state, rooms[room].com];
-            io.to(room).emit('getState', data);
+            rooms[room].comment = Object.values(rooms[room].move)[0] + "'s turn!"
+            rooms[room].state = ["", "", "", "", "", "", "", "", ""]
+            io.to(room).emit('getState', data)
+        } else if (Object.keys(rooms[room].move)[0] != socket.id && Object.keys(rooms[room].users).length >= 2) {
+            data =[rooms[room].state, rooms[room].comment]
+            io.to(room).emit('getState', data)
+        } else {
+            io.to(room).emit('updateReset')
+            rooms[room].comment = "Waiting for opponent..."
+            rooms[room].state = ["", "", "", "", "", "", "", "", ""]
+            data =[rooms[room].state, rooms[room].comment]
+            io.to(room).emit('getState', data)
         }
     })
     
     socket.on('new-user', (room, name) => {
-        socket.join(room);
-        rooms[room].users[socket.id] = name;
-        if(Object.keys(rooms[room].users).length == 1){
-            rooms[room].player1[socket.id] = name;
-            rooms[room].move[socket.id] = name;
-        }
-        if(Object.keys(rooms[room].users).length == 2)
-            rooms[room].player2[socket.id] = name;
+        socket.join(room)
+        rooms[room].users[socket.id] = name
+        //If needed assign as player
+        if (Object.keys(rooms[room].users).length == 1) {
+            rooms[room].player1[socket.id] = name
+            rooms[room].move[socket.id] = name
+        } else if (Object.keys(rooms[room].users).length == 2)
+            rooms[room].player2[socket.id] = name
 
-        if(room != '/' && Object.keys(rooms[room].users).length <= 2){
-            io.to(room).emit('updateReset');
+        //clean current state of game or emit it to new user
+        if (room != '/' && Object.keys(rooms[room].users).length <= 2) {
+            io.to(room).emit('updateReset')
             rooms[room].state = ["", "", "", "", "", "", "", "", ""]
-            if(Object.keys(rooms[room].users).length == 1)
-                rooms[room].com = "Waiting for opponent...";
-            if(Object.keys(rooms[room].users).length == 2)
-                rooms[room].com = Object.values(rooms[room].move)[0] + "'s turn!";
             
-            data =[rooms[room].state, rooms[room].com];
+            if (Object.keys(rooms[room].users).length == 1)
+                rooms[room].comment = "Waiting for opponent..."
+            else if (Object.keys(rooms[room].users).length == 2)
+                rooms[room].comment = Object.values(rooms[room].move)[0] + "'s turn!"
+            
+            data =[rooms[room].state, rooms[room].comment]
             io.to(room).emit('getState', data)
-        }
-        else if(room != '/' && Object.keys(rooms[room].users).length > 2){
-            data =[rooms[room].state, rooms[room].com];
-            io.to(room).emit('getState', data);
+        } else if (room != '/' && Object.keys(rooms[room].users).length > 2) {
+            data =[rooms[room].state, rooms[room].comment]
+            io.to(room).emit('getState', data)
         }
     })
 
     socket.on('disconnect', () => {
+        //Check every room if user was player fix stuff
         getUserRooms(socket).forEach(room => {
-            if(socket.id == Object.keys(rooms[room].player1)){
+            if (socket.id == Object.keys(rooms[room].player1)) {
                 rooms[room].player1 = rooms[room].player2
                 rooms[room].move = rooms[room].player1
                 var temp = {}
-                rooms[room].player2 = temp; 
-                rooms[room].com = "Waiting for opponent...";
-                rooms[room].state = ["", "", "", "", "", "", "", "", ""];
-                data =[rooms[room].state, rooms[room].com];
-                io.to(room).emit('getState', data);
-            }
-            else if(socket.id == Object.keys(rooms[room].player2)){
+                rooms[room].player2 = temp 
+                rooms[room].comment = "Waiting for opponent..."
+                rooms[room].state = ["", "", "", "", "", "", "", "", ""]
+                data =[rooms[room].state, rooms[room].comment]
+                io.to(room).emit('getState', data)
+            } else if(socket.id == Object.keys(rooms[room].player2)) {
                 rooms[room].move = rooms[room].player1
                 var temp = {}
-                rooms[room].player2 = temp;
-                rooms[room].com = "Waiting for opponent...";
-                rooms[room].state = ["", "", "", "", "", "", "", "", ""];
-                data =[rooms[room].state, rooms[room].com];
-                io.to(room).emit('getState', data); 
+                rooms[room].player2 = temp
+                rooms[room].comment = "Waiting for opponent..."
+                rooms[room].state = ["", "", "", "", "", "", "", "", ""]
+                data =[rooms[room].state, rooms[room].comment]
+                io.to(room).emit('getState', data) 
             }
             delete rooms[room].users[socket.id]
         })
-    });
-});
+    })
+})
   
 function getUserRooms(socket){
     return Object.entries(rooms).reduce((names, [name, room]) => {
-        if(room.users[socket.id] != null)
-            names.push(name);
-        return names;
+        if (room.users[socket.id] != null)
+            names.push(name)
+        return names
     }, [])
 }
 
-server.listen(3000);
+server.listen(3000)
